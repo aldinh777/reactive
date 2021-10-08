7a0d1c05178f8f525def51237219afac15edb273
aldinh777 <aldi.nh789@gmail.com>
2021-10-09 02:25:10 +0800

    simplify import

--- index.ts
+++ index.ts
@@ -1,24 +1,8 @@
-import { reactive } from './src/helper';
+export { reactive as default } from './src/helper';
 
-export {
-    Reactive,
-    ReactiveEvent,
-    ReactiveUpdater,
-    ReactiveCondition,
-    Rule,
-    Unsubscriber
-} from './src/Reactive';
-export {
-    reactive,
-    onChange,
-    when,
-    update,
-    increase,
-    decrease
-} from './src/helper';
-export { Duck, DuckType, duck, duckFrom } from './src/Duck';
-export { Reduck, ReduckType, ReduckListener, reduck, reduckFrom } from './src/Reduck';
-export { DinoFunction, dino } from './src/dino';
-export { quack, canQuack } from './src/util';
-
-export default reactive;
+export * from './src/Reactive';
+export * from './src/helper';
+export * from './src/Duck';
+export * from './src/Reduck';
+export * from './src/dino';
+export * from './src/util';
