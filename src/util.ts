4ec6580276f8514858f6860979b1d3d5bf10b516
aldinh777 <aldi.nh789@gmail.com>
2021-11-17 00:40:10 +0800

    recollection function to create the mighty "Reactive Collection"

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import { ReactiveArray } from './collection/ReactiveArray';
+import { ReactiveCollection } from './collection/ReactiveCollection';
+import { ReactiveMap } from './collection/ReactiveMap';
 import { Reactive, Rule } from './Reactive';
 
 export function removeFromArray<T>(elem: T, array: T[]): void {
@@ -9,4 +12,25 @@ export function removeFromArray<T>(elem: T, array: T[]): void {
 
 export function reactive<T>(initial?: T | Rule<T>, ...subscriptions: Reactive<any>[]): Reactive<T> {
     return new Reactive(initial, ...subscriptions);
+}
+
+export function recollection(obj: any, rawdata: boolean = false): ReactiveCollection<any> {
+    let result: ReactiveCollection<any>;
+    if (obj instanceof Array) {
+        result = new ReactiveArray(...obj.map(o => recollection(o, true)));
+    } else if (typeof obj === 'object') {
+        const map = new ReactiveMap();
+        for (const key in obj) {
+            const value = obj[key];
+            map.set(key, recollection(value, true));
+        }
+        result = map;
+    } else {
+        return obj;
+    }
+    if (rawdata) {
+        return result;
+    } else {
+        return result.toProxy();
+    }
 }
