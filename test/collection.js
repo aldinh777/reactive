ef7a4efe0343eb15608804e7dd43b9caeddc36f2
aldinh777 <aldi.nh789@gmail.com>
2022-08-25 06:08:52 +0800

    update testing

---
+++ test/collection.js
@@ -0,0 +1,197 @@
+const { equal, fail, deepEqual } = require('assert');
+const { stateList, stateMap } = require('../collection');
+
+const sum = (prev, next) => prev + next;
+
+describe('State List', function () {
+    const list = stateList([1, 2, 3, 4, 5]);
+    it('Update', function () {
+        let updateCounter = 0;
+        let nextSum = 0;
+        let prevSum = 0;
+        list.onUpdate((_, next, prev) => {
+            updateCounter++;
+            nextSum += next;
+            prevSum += prev;
+        });
+        const array = list.toArray();
+        for (let i = 0; i < array.length; i++) {
+            list[i]++;
+        }
+        equal(updateCounter, array.length);
+        equal(prevSum, [1, 2, 3, 4, 5].reduce(sum));
+        equal(nextSum, [2, 3, 4, 5, 6].reduce(sum))
+    });
+    describe('Delete', function () {
+        // current value = [2, 3, 4, 5, 6]
+        let deleteCounter = 0;
+        let deleteSum = 0;
+        list.onDelete((_, deleted) => {
+            deleteCounter++;
+            deleteSum += deleted;
+        });
+        it('splice', function () {
+            // aftermath [2, 5, 6] | spliced [3, 4]
+            const spliced = list.splice(1, 2);
+            equal(spliced.length, 2);
+            deepEqual(spliced, [3, 4]);
+        });
+        it('pop', function () {
+            // aftermath [2, 5] | popped -> 6
+            const popped = list.pop();
+            equal(popped, 6);
+        });
+        it('shift', function () {
+            // aftermath [5] | shifted -> 2
+            const shifted = list.shift();
+            equal(shifted, 2);
+        });
+        it('aftermath', function () {
+            // deleteds [2, 3, 4, 6]
+            equal(deleteCounter, 4);
+            equal(deleteSum, [2, 3, 4, 6].reduce(sum));
+            // current [5]
+            equal(list[0], 5);
+        });
+    });
+    describe('Insert', function () {
+        // current [5]
+        let insertCounter = 0;
+        let insertSum = 0;
+        list.onInsert((_, inserted) => {
+            insertCounter++;
+            insertSum += inserted;
+        });
+        it('push', function () {
+            // aftermath [5, 1, 2] | newlength -> 3
+            let newlength = list.push(1, 2);
+            equal(newlength, 3);
+        });
+        it('unshift', function () {
+            // aftermath [3, 4, 5, 1, 2] | newlength -> 5
+            newlength = list.unshift(3, 4);
+            equal(newlength, 5);
+        });
+        it('splice', function () {
+            // aftermath [3, 4, 5, 6, 7, 1, 2]
+            list.splice(3, 0, 6, 7);
+        });
+        it('aftermath', function () {
+            const expected = [3, 4, 5, 6, 7, 1, 2];
+            const inserteds = [1, 2, 3, 4, 6, 7];
+            deepEqual(list.toArray(), expected);
+            equal(insertCounter, inserteds.length);
+            equal(insertSum, inserteds.reduce(sum));
+        });
+    });
+    describe('Unsubscribe', function () {
+        const testlist = stateList([1, 2, 3, 4, 5]);
+        it('update', function () {
+            const updsubs = testlist.onUpdate(() => fail('List Update Listener not removed'));
+            updsubs.unsub();
+            testlist[0] = 0;
+        });
+        it('delete', function () {
+            const delsubs = testlist.onDelete(() => fail('List Delete Listener not removed'))
+            delsubs.unsub();
+            testlist.splice(0, 1);
+            testlist.pop();
+            testlist.shift();
+        });
+        it('insert', function () {
+            const inssubs = testlist.onDelete(() => fail('List Insert Listener not removed'))
+            inssubs.unsub();
+            testlist.push(0);
+            testlist.unshift(0);
+            testlist.splice(0, 0, 0);
+        });
+    });
+    describe('Resubscribe', function () {
+        const testlist = stateList([1, 2, 3, 4, 5]);
+        it('update', async function (done) {
+            const updsubs = testlist.onUpdate(() => done());
+            updsubs.unsub();
+            updsubs.resub();
+            testlist[0] = 0;
+        });
+        it('delete', function (done) {
+            const delsubs = testlist.onDelete(() => done());
+            delsubs.unsub();
+            delsubs.resub();
+            testlist.pop();
+        });
+        it('insert', function (done) {
+            const inssubs = testlist.onInsert(() => done());
+            inssubs.unsub();
+            inssubs.resub();
+            testlist.push(0);
+        });
+    });
+});
+
+describe('State Map', function () {
+    const map = stateMap({ a: 0 });
+    it('Update', async function (done) {
+        map.onUpdate((key, val) => {
+            if (key === 'a' && val === 10) {
+                done();
+            }
+        });
+        map.a = 10;
+    });
+    it('Insert', async function (done) {
+        map.onInsert((key, val) => {
+            if (key === 'moo' && val === 20) {
+                done();
+            }
+        });
+        map.moo = 20;
+    });
+    it('Delete', async function (done) {
+        map.onDelete((key, val) => {
+            if (key === 'moo' && val === 20) {
+                done();
+            }
+        });
+        map.delete('moo');
+    });
+    describe('Unsubscribe', function () {
+        const testmap = stateMap({ a: 0 });
+        it('update', function () {
+            const sub = testmap.onUpdate(() => fail('Map Update Listener not removed'));
+            sub.unsub();
+            testmap.a = 10;
+        });
+        it('insert', function () {
+            const sub = testmap.onInsert(() => fail('Map Insert Listener not removed'));
+            sub.unsub();
+            testmap.moo = 20;
+        });
+        it('delete', function () {
+            const sub = testmap.onDelete(() => fail('Map Delete Listener not removed'));
+            sub.unsub();
+            testmap.delete('moo');
+        });
+    });
+    describe('Resubscribe', function () {
+        const testmap = stateMap({ a: 0 });
+        it('update', async function (done) {
+            const sub = testmap.onUpdate(() => done());
+            sub.unsub();
+            sub.resub();
+            testmap.a = 10;
+        });
+        it('insert', function (done) {
+            const sub = testmap.onInsert(() => done());
+            sub.unsub();
+            sub.resub();
+            testmap.moo = 20;
+        });
+        it('delete', function (done) {
+            const sub = testmap.onDelete(() => done());
+            sub.unsub();
+            sub.resub();
+            testmap.delete('moo');
+        });
+    })
+});
--- test/reactive.js
+++ test/reactive.js
@@ -2,116 +2,120 @@ const { equal, fail, notEqual } = require('assert');
 const { state, observe } = require('..');
 
 describe('Reactivity', function () {
+    const hello = state('');
+    const undef = state();
     it('Initialization', function () {
-        const a = state('hello');
-        const undef = state();
-        equal(a.value, 'hello');
+        hello.value = 'hello';
+        equal(hello.value, 'hello');
         equal(undef.value, undefined);
     });
     it('Value Update', function () {
-        const a = state('hello');
-        a.value = 'hi';
-        equal(a.value, 'hi');
-        a.value = undefined;
-        equal(a.value, undefined);
+        hello.value = 'hi';
+        equal(hello.value, 'hi');
+        hello.value = undefined;
+        equal(hello.value, undefined);
     });
     it('Subscription', function () {
-        const a = state('hello');
-        const b = state('');
-        observe(a, val => b.value = val + ' world!')
-        equal(b.value, 'hello world!');
-        a.value = 'hi';
-        equal(b.value, 'hi world!');
-        b.value = 'reset';
-        equal(b.value, 'reset');
-    });
-    it('External Subscription', function () {
-        // Reactive not even required!!!
-        let x = 'hello';
-        const a = () => x + ' world!';
-        equal(a(), 'hello world!');
-        x = 'hi';
-        equal(a(), 'hi world!');
+        hello.value = 'hello';
+        const helloWorld = state('');
+        observe(hello, val => helloWorld.value = val + ' world!')
+        equal(helloWorld.value, 'hello world!');
+        hello.value = 'hi';
+        equal(helloWorld.value, 'hi world!');
     });
 });
 
 describe('Observability', function () {
     it('Observation', async function (done) {
-        const a = state('hello');
-        a.addListener(val => {
+        const hello = state('hello');
+        hello.addListener(val => {
             equal(val, 'yes');
             done();
         });
-        a.value = 'yes';
+        hello.value = 'yes';
     });
     it('Subscriber Update', async function (done) {
-        const a = state('hello');
-        const b = state('');
-        observe(a, val => b.value = val + ' master!')
-        b.addListener(val => {
+        const hello = state('hello');
+        const helloMaster = state('');
+        observe(hello, val => helloMaster.value = val + ' master!')
+        helloMaster.addListener(val => {
             equal(val, 'yes master!');
             done();
         });
-        a.value = 'yes';
+        hello.value = 'yes';
     });
     it('Immediate Observe', async function (done) {
-        const a = state('hello');
-        observe(a, val => {
+        const hello = state('hello');
+        observe(hello, val => {
             equal(val, 'hello');
             done();
         });
     });
     it('Conditional Observe', async function (done) {
-        const a = state('hello');
-        a.addListener(val => {
+        const hello = state('hello');
+        hello.addListener(val => {
             if (val.length === 6) {
                 equal(val.length, 6);
                 done();
             }
         });
-        a.value = 'ninja';
-        a.value = 'hatori';
+        hello.value = 'ninja';
+        hello.value = 'hatori';
     });
     it('On Equals', async function (done) {
-        const a = state('hello');
-        observe(a, val => {
+        const hello = state('hello');
+        observe(hello, val => {
             if (val === 'hello') {
                 equal(val, 'hello');
                 done();
             }
         });
     });
-    it('Value Binding', function () {
+    it('Object Property Binding', function () {
         const obj = { attr1: 'jazzie', attr2: 'joggie' };
-        const a = state('hello');
-        observe(a, val => {
+        const hello = state('hello');
+        observe(hello, val => {
             obj.attr1 = val;
             obj.attr2 = val + ' world!';
         });
-        equal(obj.attr1, a.value);
+        equal(obj.attr1, hello.value);
         equal(obj.attr2, 'hello world!');
-        a.value = 'yahoo';
+        hello.value = 'yahoo';
         equal(obj.attr1, 'yahoo');
         equal(obj.attr2, 'yahoo world!');
     });
     it('Unsubscribe', function () {
         const obj = { attr: 'jazzie' };
-        const a = state('hello');
-        const update = a.addListener(() => fail());
-        update.unsubscribe();
-        a.value = 'hi';
-        a.value = 'hatori';
-        notEqual(obj.attr, a.value);
+        const hello = state('hello');
+        const sub = hello.addListener((val) => {
+            obj.attr = val;
+            fail('State not unsubscribed');
+        });
+        sub.unsub();
+        hello.value = 'hi';
+        hello.value = 'hatori';
+        notEqual(obj.attr, hello.value);
+    });
+    it('Resubscribe', function () {
+        let samp = '';
+        const hello = state('hello');
+        const sub = hello.addListener(val => {
+            samp = val;
+        });
+        sub.unsub();
+        sub.resub();
+        hello.value = 'moola'
+        equal(samp, hello.value);
     });
     it('Old Value Checking', function () {
-        const a = state('hello');
-        let prev = a.value;
-        a.onChange((_next, oldValue) => {
+        const hello = state('hello');
+        let prev = hello.value;
+        hello.onChange((_next, oldValue) => {
             equal(oldValue, prev);
         });
-        a.value = 'story';
-        prev = a.value;
-        a.value = 'jojoh';
+        hello.value = 'story';
+        prev = hello.value;
+        hello.value = 'jojoh';
     });
     it('Update Cancellation', function () {
         const a = state('hello');
