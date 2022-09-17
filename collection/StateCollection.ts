6b438288c718f135dfe1fd361fe5702a5d24ccc8
aldinh777 <aldi.nh789@gmail.com>
2022-09-17 16:43:32 +0800

    pointless timewasting type fixing and documentations. for no absolute reason

--- collection/StateCollection.ts
+++ collection/StateCollection.ts
@@ -4,17 +4,13 @@ export type UpdateListener<K, V> = (index: K, next: V, previous: V) => any;
 export type InsertListener<K, V> = (index: K, inserted: V) => any;
 export type DeleteListener<K, V> = (index: K, deleted: V) => any;
 export type ResizeListener<K, V> = (type: '+' | '-', index: K, item: V) => any;
-export type CollectSubscription<K, V, C, U> = Subscription<StateCollection<K, V, C>, U>;
 
 /**
- * - K -> Key | Index Type.
- * - V -> Value Type.
- * - R -> Raw Collection Type.
+ * Abstract class to be extended as an actual observable collection
  *
- * ex: StateList of string makes :
- * - K: number as it's index
- * - V: string as it's output
- * - R: string[] to store it's raw data internally
+ * - K -> Key | Index Type: (ex. number, string).
+ * - V -> Value Type: (ex. number, string, object).
+ * - R -> Raw Collection Type: (ex. string[], Map<string, string>).
  */
 export abstract class StateCollection<K, V, R> {
     protected _upd: UpdateListener<K, V>[] = [];
@@ -24,16 +20,24 @@ export abstract class StateCollection<K, V, R> {
 
     abstract get(index: K): V | undefined;
     abstract set(index: K, value: V): this;
-    onUpdate(listener: UpdateListener<K, V>): CollectSubscription<K, V, R, UpdateListener<K, V>> {
+    onUpdate(
+        listener: UpdateListener<K, V>
+    ): Subscription<StateCollection<K, V, R>, UpdateListener<K, V>> {
         return createSubscription(this, listener, this._upd);
     }
-    onInsert(listener: InsertListener<K, V>): CollectSubscription<K, V, R, InsertListener<K, V>> {
+    onInsert(
+        listener: InsertListener<K, V>
+    ): Subscription<StateCollection<K, V, R>, InsertListener<K, V>> {
         return createSubscription(this, listener, this._ins);
     }
-    onDelete(listener: DeleteListener<K, V>): CollectSubscription<K, V, R, DeleteListener<K, V>> {
+    onDelete(
+        listener: DeleteListener<K, V>
+    ): Subscription<StateCollection<K, V, R>, DeleteListener<K, V>> {
         return createSubscription(this, listener, this._del);
     }
-    onResize(listener: ResizeListener<K, V>): CollectSubscription<K, V, R, ResizeListener<K, V>> {
+    onResize(
+        listener: ResizeListener<K, V>
+    ): Subscription<StateCollection<K, V, R>, ResizeListener<K, V>> {
         const subscriptions = [
             this.onInsert((index, inserted) => listener('+', index, inserted)),
             this.onDelete((index, deleted) => listener('-', index, deleted))
--- util/index.ts
+++ util/index.ts
@@ -1,10 +1,21 @@
+/** Object That store subscriptions information */
 export interface Subscription<S, T> {
+    /** observed item */
     target: S;
+    /** listener that listen when update happen */
     listener: T;
+    /** disable listener to watch update */
     unsub(): any;
+    /** reenable listener to watch update */
     resub(): any;
 }
 
+/**
+ * remove item from an array if item exists in that array
+ *
+ * @param elem item to remove from array
+ * @param array array that stored said item
+ */
 function removeFromArray<T>(elem: T, array: T[]): void {
     const index = array.indexOf(elem);
     if (index !== -1) {
@@ -12,13 +23,27 @@ function removeFromArray<T>(elem: T, array: T[]): void {
     }
 }
 
+/**
+ * push item to an array if item not exists in that array
+ *
+ * @param elem item to push to array
+ * @param array array to be pushed
+ */
 function pushNonExists<T>(elem: T, array: T[]): void {
     if (!array.includes(elem)) {
         array.push(elem);
     }
 }
 
-export function createSubscription<S, T>(target: S, listener: T, array: T[]): Subscription<S, T> {
+/**
+ * create subscription object that control listener behaviour
+ *
+ * @param target item being observed
+ * @param listener listener to be added to array
+ * @param array array that stores listeners
+ * @returns object literals that store subscription informations
+ */
+export function createSubscription<T, L>(target: T, listener: L, array: L[]): Subscription<T, L> {
     pushNonExists(listener, array);
     return {
         target: target,
@@ -32,11 +57,19 @@ export function createSubscription<S, T>(target: S, listener: T, array: T[]): Su
     };
 }
 
-export function createMultiSubscriptions<S, T, X, U>(
-    target: S,
-    listener: T,
-    subscriptions: Subscription<X, U>[]
-): Subscription<S, T> {
+/**
+ * create subscription object that control another subscriptions behaviour
+ *
+ * @param target item or items being observed
+ * @param listener callback when update happen
+ * @param subscriptions subscriptions to be handled
+ * @returns object literals that store subscription information
+ */
+export function createMultiSubscriptions<T, L, S extends Subscription<any, any>>(
+    target: T,
+    listener: L,
+    subscriptions: S[]
+): Subscription<T, L> {
     return {
         target: target,
         listener: listener,
