1fef87a156f216c546453b0b407f23049726f5a7
aldinh777 <aldi.nh789@gmail.com>
2022-10-14 19:05:19 +0800

    simple cmd file to quickly clear compiled files

---
+++ clean.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+:: Utils
+del util\*.d.ts
+del util\*.js
+del util\collection\*.d.ts
+del util\collection\*.js
+
+:: State
+del state\*.d.ts
+del state\*.js
+
+:: Collections
+del collection\*.d.ts
+del collection\*.js
+del collection\list\*.d.ts
+del collection\list\*.js
+del collection\map\*.d.ts
+del collection\map\*.js
