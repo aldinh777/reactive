6be98b82daa3e5d46d8b3dfa1706824da648879c
aldinh777 <aldi.nh789@gmail.com>
2023-01-28 04:39:10 +0800

    fix listener recursion at setValue

--- state/State.ts
+++ state/State.ts
@@ -5,9 +5,21 @@ export type ChangeHandler<T> = (next: T, previous: T) => any;
 export type StateSubscription<T> = Subscription<State<T>, UpdateListener<T>>;
 
 export class State<T> {
+    /** List of active update listeners */
     private _upd: UpdateListener<T>[] = [];
+    /** The actual value being stored */
     private _val: T;
-    private _lock: boolean = false;
+    /**
+     *  Update Lock :   Prevent state from executing another listener
+     *                  while it is currently executing listener to
+     *                  prevent infinite recursion
+     */
+    private _ulock: boolean = false;
+    /**
+     *  Handler Lock :  Prevent next listeners from being executed
+     *                  and as a marker
+     */
+    private _hlock: boolean = false;
 
     constructor(initial: T) {
         this._val = initial;
@@ -17,20 +29,25 @@ export class State<T> {
     }
     setValue(value: T) {
         this._val = value;
-        while (!this._lock) {
-            this._lock = true;
+        this._hlock = this._ulock;
+        while (!this._ulock) {
+            this._ulock = true;
             for (const listener of this._upd) {
                 listener(this._val);
-                if (!this._lock) {
+                if (this._hlock) {
                     break;
                 }
             }
+            this._ulock = !this._hlock;
+            this._hlock = false;
         }
-        this._lock = false;
+        this._ulock = this._hlock;
     }
+    /** Add listener to be called when the value are updated */
     addListener(listener: UpdateListener<T>): StateSubscription<T> {
         return createSubscription(this, listener, this._upd);
     }
+    /** Add listener to be called only when the value are changed */
     onChange(handler: ChangeHandler<T>): StateSubscription<T> {
         let oldValue = this.getValue();
         return this.addListener((value) => {
