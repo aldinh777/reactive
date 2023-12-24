dc7f7d016dd2dc2b3da6b64a66fd8cc7c526baa0
aldinh777 <aldi.nh789@gmail.com>
2023-12-25 04:28:15 +0800

    functify map, filter, and sort function

---
+++ collection/list/filter.ts
@@ -0,0 +1,102 @@
+import { Watchable, watchify } from '../../helper/collection';
+import { RList } from '../list';
+
+interface RListFilter<T> extends Watchable<number, T> {
+    (): T[];
+    (index: number): T;
+    replaceFilter(filter: (item: T) => boolean): void;
+}
+
+export function filterlist<T>(list: RList<T>, filter: (item: T) => boolean): RListFilter<T> {
+    const raw: T[] = [];
+    const _f: boolean[] = list().map(filter);
+    for (let i = 0; i < _f.length; i++) {
+        if (_f[i]) {
+            raw.push(list(i));
+        }
+    }
+    function updateFiltered(index: number, value: T, prev: T): void {
+        const fIndex = findFilteredIndex(index);
+        raw[fIndex] = value;
+        trigger('=', fIndex, value, prev);
+    }
+    function insertFiltered(index: number, value: T): void {
+        const fIndex = findFilteredIndex(index);
+        raw.splice(fIndex, 0, value);
+        trigger('+', fIndex, value);
+    }
+    function deleteFiltered(index: number, value: T): void {
+        const fIndex = findFilteredIndex(index);
+        const prev = raw[fIndex];
+        raw.splice(fIndex, 1);
+        trigger('-', fIndex, prev);
+    }
+    function findFilteredIndex(sourceIndex: number): number {
+        let filteredIndex = 0;
+        for (let i = 0; i < sourceIndex; i++) {
+            if (_f[i]) {
+                filteredIndex++;
+            }
+        }
+        return filteredIndex;
+    }
+    list.onUpdate((index, value, prev) => {
+        const allowAfter = filter(value);
+        if (_f[index] !== allowAfter) {
+            _f[index] = allowAfter;
+            if (allowAfter) {
+                insertFiltered(index, value);
+            } else {
+                deleteFiltered(index, value);
+            }
+        } else if (allowAfter) {
+            updateFiltered(index, value, prev);
+        }
+    });
+    list.onInsert((index, value) => {
+        const allow = filter(value);
+        _f.splice(index, 0, allow);
+        if (allow) {
+            insertFiltered(index, value);
+        }
+    });
+    list.onDelete((index, value) => {
+        if (_f[index]) {
+            deleteFiltered(index, value);
+        }
+        _f.splice(index, 1);
+    });
+    const RListFilter = (index?: number) => {
+        if (index === undefined) {
+            return raw;
+        }
+        return raw[index];
+    };
+    const trigger = watchify(RListFilter);
+    RListFilter.replaceFilter = (newFilter: (item: T) => boolean): void => {
+        filter = newFilter;
+        const next_f = list().map(newFilter);
+        let currentIndex = 0;
+        for (let i = 0; i < next_f.length; i++) {
+            const allowNext = next_f[i];
+            if (_f[i] !== allowNext) {
+                if (allowNext) {
+                    const inserted = list(i);
+                    raw.splice(currentIndex, 0, inserted);
+                    trigger('+', currentIndex, inserted);
+                    currentIndex++;
+                } else {
+                    const deleted = raw[currentIndex];
+                    raw.splice(currentIndex, 1);
+                    trigger('-', currentIndex, deleted);
+                    currentIndex = currentIndex && currentIndex - 1;
+                }
+            } else if (allowNext) {
+                currentIndex++;
+            }
+        }
+        _f.length = 0;
+        _f.push(...next_f);
+    };
+    return RListFilter as RListFilter<T>;
+}
---
+++ collection/list/sort.ts
@@ -0,0 +1,78 @@
+import { Watchable, watchify } from '../../helper/collection';
+import { RList } from '../list';
+
+interface RListSort<T> extends Watchable<number, T> {
+    (): T[];
+    (index: number): T;
+    replaceSorter(sorter: (item: T, compare: T) => boolean): void;
+}
+
+export function sortlist<T>(list: RList<T>, sorter: (item: T, compare: T) => boolean) {
+    const raw: T[] = [];
+    function insertItem(array: T[], item: T): number {
+        let insertIndex = array.length;
+        for (let i = 0; i < array.length; i++) {
+            const elem = array[i];
+            if (sorter(item, elem)) {
+                insertIndex = i;
+                break;
+            }
+        }
+        array.splice(insertIndex, 0, item);
+        return insertIndex;
+    }
+    for (const item of list()) {
+        insertItem(raw, item);
+    }
+    list.onUpdate((_, value, prev) => {
+        const prevIndex = raw.indexOf(prev);
+        if (prevIndex !== -1) {
+            raw.splice(prevIndex, 1);
+            const nextIndex = insertItem(raw, value);
+            if (prevIndex === nextIndex) {
+                trigger('=', nextIndex, value, prev);
+            } else {
+                trigger('-', prevIndex, prev);
+                trigger('+', nextIndex, value);
+            }
+        }
+    });
+    list.onInsert((_, value) => {
+        const insertIndex = insertItem(raw, value);
+        trigger('+', insertIndex, value);
+    });
+    list.onDelete((_, value) => {
+        const index = raw.indexOf(value);
+        if (index !== -1) {
+            raw.splice(index, 1);
+            trigger('-', index, value);
+        }
+    });
+    const RListSort = (index?: number) => {
+        if (index === undefined) {
+            return raw;
+        }
+        return raw[index];
+    };
+    const trigger = watchify(RListSort);
+    RListSort.replaceSorter = (sort: (item: T, elem: T) => boolean) => {
+        sorter = sort;
+        const sorted: T[] = [];
+        for (const item of list()) {
+            insertItem(sorted, item);
+        }
+        for (let i = 0; i < sorted.length; i++) {
+            const item = sorted[i];
+            const elem = raw[i];
+            if (item !== elem) {
+                const deleteIndex = raw.indexOf(item);
+                const swap = raw[deleteIndex];
+                raw.splice(deleteIndex, 1);
+                trigger('-', deleteIndex, swap);
+                raw.splice(i, 0, swap);
+                trigger('+', i, swap);
+            }
+        }
+    };
+    return RListSort as RListSort<T>;
+}
