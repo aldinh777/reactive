1f08e4a9a234c193169142e191ae11680c8f642a
aldinh777 <aldi.nh789@gmail.com>
2024-04-13 19:19:57 +0800

    adding unsubscribe for thingies and rename thingies also doumentation thingy

--- state/internal.ts
+++ state/internal.ts
@@ -1,11 +1,14 @@
 import { State } from './index.js';
 
 /**
- * Weak Map that stores dependencies of each state to prevent
+ * Weak Map that stores the root dependencies of each state to prevent
  * any state from having duplicate dependency or parent dependency
  */
 export const __ROOT_LIST = new WeakMap<State, State[]>();
 export const __MUTATED_DATA = {
     _isExecuting: false,
+    /**
+     * The set of states that is being used while `mutated` or `effect` method is being executing
+     */
     _dependencies: new Set<State>()
 };
