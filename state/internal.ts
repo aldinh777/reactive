72ac552ab9d9b512a6e36a784e521063e9963b5b
aldinh777 <aldi.nh789@gmail.com>
2024-04-16 20:09:48 +0800

    AAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- state/internal.ts
+++ state/internal.ts
@@ -1,10 +1,11 @@
+import { Unsubscribe } from '../utils/subscription.js';
 import { State } from './index.js';
 
 /**
  * Weak Map that stores the root dependencies of each state to prevent
  * any state from having duplicate dependency or parent dependency
  */
-export const __ROOT_LIST = new WeakMap<State, State[]>();
+export const __ROOT_SET = new WeakMap<State, Map<State, Unsubscribe>>();
 export const __MUTATED_DATA = {
     _isExecuting: false,
     /**
--- utils/index.ts
+++ utils/index.ts
@@ -1,92 +1,76 @@
 import type { State } from '../state/index.js';
 import type { Unsubscribe } from './subscription.js';
-import { pushNonExists } from '@aldinh777/toolbox/array-operation.js';
-import { __ROOT_LIST, __MUTATED_DATA } from '../state/internal.js';
+import { __ROOT_SET, __MUTATED_DATA } from '../state/internal.js';
 import { state } from '../state/index.js';
 
 export interface MutatedState<T> extends State<T> {
     stop(): void;
 }
 
-function filterDependency(states: State[]) {
-    const deps: State[] = [];
+function filterDeps(states: Set<State>) {
+    const deps = new Set<State>();
     for (const dep of states) {
-        if (__ROOT_LIST.has(dep)) {
-            const rl = __ROOT_LIST.get(dep)!;
-            for (const root of rl) {
-                pushNonExists(deps, root);
+        if (__ROOT_SET.has(dep)) {
+            const rootSet = __ROOT_SET.get(dep)!;
+            for (const [root] of rootSet) {
+                deps.add(root);
             }
         } else {
-            pushNonExists(deps, dep);
+            deps.add(dep);
         }
     }
     return deps;
 }
 
-export const effectFrom = <T>(...states: State<T>[]) => {
-    const deps = filterDependency(states);
-    return (handler: (...values: T[]) => any, execute = true): Unsubscribe => {
-        const exec = () => handler(...states.map((s) => s()));
-        const unsubs: Unsubscribe[] = [];
-        for (const dep of deps) {
-            dep.onChange(exec);
-        }
-        if (execute) {
-            exec();
-        }
-        return () => {
-            for (const unsub of unsubs) {
-                unsub();
-            }
-        };
-    };
-};
-
-export const mutatedFrom = <T>(...states: State<T>[]) => {
-    const dependencies = filterDependency(states);
-    return <U>(handler: (...values: T[]) => U, execute = true): MutatedState<U> => {
-        const st = state<U>() as MutatedState<U>;
-        const exec = () => st(handler(...states.map((s) => s())));
-        const unsubs: Unsubscribe[] = [];
-        for (const dep of dependencies) {
-            unsubs.push(dep.onChange(exec));
-        }
-        if (execute) {
-            exec();
-        }
-        __ROOT_LIST.set(st, dependencies);
-        st.stop = () => {
-            for (const unsub of unsubs) {
-                unsub();
-            }
-            __ROOT_LIST.delete(st);
-        };
-        st.toString = () => `Mutated { value: ${st()} }`;
-        return st;
-    };
-};
-
-export const effect = (effectHandler: () => any): Unsubscribe => {
+function handleEffect<T>(effectHandler: () => T, state?: MutatedState<T>): [() => void, Unsubscribe] {
     if (__MUTATED_DATA._isExecuting) {
         throw Error('nested mutated or effect are not allowed');
     }
-    __MUTATED_DATA._isExecuting = true;
-    effectHandler();
-    const unsub = effectFrom(...__MUTATED_DATA._dependencies)(effectHandler, false);
-    __MUTATED_DATA._isExecuting = false;
-    __MUTATED_DATA._dependencies.clear();
-    return unsub;
+    const rootDepsMap = new Map<State, Unsubscribe>();
+    if (state) {
+        __ROOT_SET.set(state, rootDepsMap);
+    }
+    const exec = () => {
+        __MUTATED_DATA._isExecuting = true;
+        const result = effectHandler();
+        const newDeps = filterDeps(__MUTATED_DATA._dependencies);
+        __MUTATED_DATA._isExecuting = false;
+        __MUTATED_DATA._dependencies.clear();
+        for (const [oldDep, unsub] of rootDepsMap) {
+            unsub();
+            if (newDeps.has(oldDep)) {
+                newDeps.delete(oldDep);
+                rootDepsMap.set(oldDep, oldDep.onChange(exec));
+            } else {
+                rootDepsMap.delete(oldDep);
+            }
+        }
+        for (const newDep of newDeps) {
+            rootDepsMap.set(newDep, newDep.onChange(exec));
+        }
+        state?.(result);
+    };
+    const stop: Unsubscribe = () => {
+        for (const [, unsub] of rootDepsMap) {
+            unsub();
+        }
+        if (state) {
+            __ROOT_SET.delete(state);
+        }
+    };
+    return [exec, stop];
+}
+
+export const setEffect = (effectHandler: () => any): Unsubscribe => {
+    const [exec, stop] = handleEffect(effectHandler);
+    exec();
+    return stop;
 };
 
 export const mutated = <T>(mutator: () => T) => {
-    if (__MUTATED_DATA._isExecuting) {
-        throw Error('nested mutated or effect are not allowed');
-    }
-    __MUTATED_DATA._isExecuting = true;
-    const result = mutator();
-    const st = mutatedFrom(...__MUTATED_DATA._dependencies)(mutator, false);
-    st(result);
-    __MUTATED_DATA._isExecuting = false;
-    __MUTATED_DATA._dependencies.clear();
+    const st = state<T>() as MutatedState<T>;
+    const [exec, stop] = handleEffect(mutator, st);
+    st.stop = stop;
+    exec();
     return st;
 };
