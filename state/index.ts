52d006fda92814cd5e28f5bcbdf360ae2e59e9f5
aldinh777 <aldi.nh789@gmail.com>
2025-01-07 22:42:39 +0800

    making sense of nonsense naming systems, add better explanation to some code, remove comments of non exported constants

--- state/index.ts
+++ state/index.ts
@@ -3,63 +3,42 @@
  * Base module that exposes definition and function to create State
  */
 
-/**
- * ====================================================================================================
- *    Internal module containing shared variables and functions to manage dependencies and effects.
- * ====================================================================================================
- */
+// ====================================================================================================
+//    Internal module containing shared variables and functions to manage dependencies and effects.
+// ====================================================================================================
 
-/**
- * Placeholder symbol for default value of state. used to differentiate states with value `undefined`.
- */
-const EMPTY = Symbol('empty');
-
-/**
- * Placeholder State to mark effect to recall, in case a circular dependency is updating itself
- */
-const RECALL_STATE = state(EMPTY);
-
-/**
- * A WeakMap that maps each state to its root dependencies. This is used
- * to prevent any state from having duplicate dependencies or parent
- * dependencies.
- */
-const ROOT_SET: WeakMap<State, Map<State, (() => void) | undefined>> = new WeakMap();
-
-/**
- * A WeakSet that stores all states created using the `computed` function.
- */
-const DYNAMICS: WeakSet<State> = new WeakSet();
-
-/**
- * An array of Sets that stores the dependencies of each stack of effects.
- */
+const PLACEHOLDER_EMPTY = Symbol('empty');
+const CIRCULAR_DETECTION_STATE = state(PLACEHOLDER_EMPTY);
+const ROOT_DEPENDENCIES_MAP: WeakMap<State, Map<State, (() => void) | undefined>> = new WeakMap();
+const DYNAMIC_STATES: WeakSet<State> = new WeakSet();
 const EFFECTS_STACK: Set<State>[] = [];
 
-/**
- * ====================================================================================================
- *                                               State
- * ====================================================================================================
- */
+// ====================================================================================================
+//                                               State
+// ====================================================================================================
 
 /**
- * A reactive state interface that provides methods to get, set, and listen to value changes.
+ * Reactive state interface that provides methods to get, set, and listen to value changes.
  */
 export interface State<T = any> {
     /**
-     * Retrieves the current state value.
+     * Retrieves current state value.
      */
     (): T;
     /**
-     * Updates the state with a new value.
+     * Updates state with a new value.
      */
     (value: T): void;
+    /**
+     * Function to get current value, override if necessary
+     */
     getValue(): T;
     /**
      * Registers a handler that will be called whenever the state changes.
-     * @param handler The function to be called when the state changes.
+     *
+     * @param handler Function to be called when the state changes.
      * @param isLast If true, the handler will be called last after all other handlers have been called.
-     * @returns An unsubscribe function to remove the handler from the list of active handlers.
+     * @returns Unsubscribe function to remove handler from the list of listeners.
      */
     onChange(handler: (next: T, previous: T) => any, isLast?: boolean): () => void;
 }
@@ -76,75 +55,71 @@ function subscribe<L>(set: Set<L>, listener: L): () => void {
  * @returns The reactive state.
  */
 export function state<T = any>(initial?: T): State<T> {
-    /** List of active update listeners */
-    const upd: Set<(value: T) => any> = new Set();
-    /** Listeners that will be executed last */
-    const updl: Set<(value: T) => any> = new Set();
-    /** The actual value being stored */
-    let val: T = initial as T;
-    /**
-     *  Update Lock :   Prevent state from executing another listener
-     *                  while it is currently executing listener to
-     *                  prevent infinite recursion
-     */
-    let ulock = false;
-    /**
-     *  Handler Lock :  Prevent next listeners from being executed
-     *                  and act as marker
-     */
-    let hlock = false;
-    const State = (next: T | typeof EMPTY = EMPTY) => {
+    const listeners: Set<(value: T) => any> = new Set();
+    const lastListeners: Set<(value: T) => any> = new Set();
+    let value: T = initial as T;
+    let isChanging = false;
+    let willChange = false;
+
+    const State = (nextValue: T | typeof PLACEHOLDER_EMPTY = PLACEHOLDER_EMPTY) => {
         const effectStack = EFFECTS_STACK.length ? EFFECTS_STACK[EFFECTS_STACK.length - 1] : null;
-        if (next === EMPTY) {
+        if (nextValue === PLACEHOLDER_EMPTY) {
             effectStack?.add(State);
             return State.getValue();
         }
-        val = next;
-        hlock = ulock;
+
+        value = nextValue;
+        willChange = isChanging; /* ref 1 */
+
         if (effectStack?.has(State)) {
-            effectStack.add(RECALL_STATE);
+            effectStack.add(CIRCULAR_DETECTION_STATE);
         }
-        while (!ulock) {
-            ulock = true;
-            for (const listener of [...upd, ...updl]) {
-                listener(val);
-                if (hlock) {
+
+        // execute listeners only when the current state are not being used, this is to prevent recursion
+        while (!isChanging) {
+            isChanging = true;
+            for (const listener of [...listeners, ...lastListeners]) {
+                listener(value); /* ref 2 */
+
+                // see ref 1, there is a chance ref 2 are invoking update of this state, thus setting the flag to true
+                if (willChange) {
                     break;
                 }
             }
-            ulock = !hlock;
-            hlock = false;
+
+            // in case the listeners loop is stopped by break statement, reset the flag so it redo the while loop
+            isChanging = !willChange;
+            willChange = false;
         }
-        ulock = hlock;
+
+        // when the while loop is over, set back the flag to false.
+        // but in case update are invoked by ref 2, then the flag will be retrieved from ref 1
+        isChanging = willChange;
     };
-    State.getValue = () => val;
-    State.onChange = (handler: (next: T, previous: T) => any, isLast = false) => {
-        let oldValue = val;
-        return subscribe(isLast ? updl : upd, (value: T) => {
+
+    State.getValue = () => value;
+    State.onChange = (handler: (next: T, prev: T) => any, isLast = false) => {
+        let oldValue = value;
+        return subscribe(isLast ? lastListeners : listeners, (value: T) => {
             if (value !== oldValue) {
                 handler(value, oldValue);
                 oldValue = value;
             }
         });
     };
+
     return State as State<T>;
 }
 
-/**
- * ====================================================================================================
- *                                      Computed and Effects
- * ===================================================================================================
- */
+// ====================================================================================================
+//                                      Computed and Effects
+// ====================================================================================================
 
-/**
- * Filters a set of states, if there is any stored dependencies from the state, use those
- * dependencies instead, if there is none, then use the state
- */
-function filterDeps(states: Set<State>): Set<State> {
+function filterDependencies(states: Set<State>): Set<State> {
     const deps = new Set<State>();
     for (const dep of states) {
-        if (ROOT_SET.has(dep)) {
-            const rootSet = ROOT_SET.get(dep)!;
+        if (ROOT_DEPENDENCIES_MAP.has(dep)) {
+            const rootSet = ROOT_DEPENDENCIES_MAP.get(dep)!;
             for (const [root] of rootSet) {
                 deps.add(root);
             }
@@ -155,13 +130,6 @@ function filterDeps(states: Set<State>): Set<State> {
     return deps;
 }
 
-/**
- * Handles the execution of an effect handler and manages dependencies.
- *
- * @param effectHandler The effect handler function to be executed.
- * @param state The state to be updated with the result of the effect handler.
- * @returns An unsubscribe function to stop the effect.
- */
 function handleEffect<T>(effectHandler: () => T, state?: State<T>): () => void {
     const rootDepsMap = new Map<State, () => void>();
     let totalObservers = 0;
@@ -187,14 +155,14 @@ function handleEffect<T>(effectHandler: () => T, state?: State<T>): () => void {
         }
         rootDepsMap.clear();
         if (state) {
-            ROOT_SET.delete(state);
+            ROOT_DEPENDENCIES_MAP.delete(state);
         }
     };
     const updateDependencies = () => {
         EFFECTS_STACK.push(new Set());
         const result = effectHandler();
         const deps = EFFECTS_STACK.pop()!;
-        if (deps.has(RECALL_STATE)) {
+        if (deps.has(CIRCULAR_DETECTION_STATE)) {
             recall = true;
             return result;
         }
@@ -205,7 +173,7 @@ function handleEffect<T>(effectHandler: () => T, state?: State<T>): () => void {
             }
             rootDepsMap.set(newDep, newDep.onChange(exec));
         }
-        const rootDeps = filterDeps(deps);
+        const rootDeps = filterDependencies(deps);
         for (const [oldDep, unsub] of rootDepsMap) {
             if (!rootDeps.has(oldDep)) {
                 unsub();
@@ -220,14 +188,14 @@ function handleEffect<T>(effectHandler: () => T, state?: State<T>): () => void {
         return result;
     };
     if (state) {
-        DYNAMICS.add(state);
+        DYNAMIC_STATES.add(state);
         const nativeGetValue = state.getValue;
         const nativeOnChange = state.onChange;
         state.getValue = () => (totalObservers > 0 ? nativeGetValue() : effectHandler());
         state.onChange = (handler: (next: T, previous: T) => any, isLast?: boolean) => {
             totalObservers++;
             if (totalObservers === 1) {
-                ROOT_SET.set(state, rootDepsMap);
+                ROOT_DEPENDENCIES_MAP.set(state, rootDepsMap);
                 exec();
             }
             const unsub = nativeOnChange(handler, isLast);
@@ -248,21 +216,13 @@ function handleEffect<T>(effectHandler: () => T, state?: State<T>): () => void {
     return unsubscribe;
 }
 
-/**
- * Handles the execution of a handler with fixed dependencies.
- *
- * @param states - The states to be used as dependencies.
- * @param effectHandler - The handler function to be executed.
- * @param state - The state to be updated with the result of the handler.
- * @returns An unsubscribe function to stop the effect.
- */
 function handleFixed<T, U>(states: State<T>[], effectHandler: (...args: T[]) => U, state?: State<U>): () => void {
     const executeEffect = () => effectHandler(...states.map((s) => s()));
-    if (states.some((st) => DYNAMICS.has(st))) {
+    if (states.some((st) => DYNAMIC_STATES.has(st))) {
         return handleEffect(executeEffect, state);
     }
     const rootDepsMap = new Map<State, (() => void) | undefined>();
-    const rootDeps = filterDeps(new Set(states));
+    const rootDeps = filterDependencies(new Set(states));
     for (const dep of rootDeps) {
         rootDepsMap.set(dep, undefined);
     }
@@ -285,7 +245,7 @@ function handleFixed<T, U>(states: State<T>[], effectHandler: (...args: T[]) => 
         }
     };
     if (state) {
-        ROOT_SET.set(state, rootDepsMap);
+        ROOT_DEPENDENCIES_MAP.set(state, rootDepsMap);
         const nativeGetValue = state.getValue;
         const nativeOnChange = state.onChange;
         state.getValue = () => (totalObservers > 0 ? nativeGetValue() : effectHandler(...states.map((s) => s())));
