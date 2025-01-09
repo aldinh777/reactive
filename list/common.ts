c6745ca8dd5f8bd56d17b2edfc59ae3701ed6f20
aldinh777 <aldi.nh789@gmail.com>
2025-01-09 17:40:38 +0800

    reduce unnecessary comment on list, update some logic

--- list/common.ts
+++ list/common.ts
@@ -3,45 +3,16 @@
  * Shared module for types and function related to watchability
  */
 
-/**
- * Represents the operation type of a watchable list.
- */
 type Operation = '+' | '-' | '=';
-
-/**
- * Represents the handler function for a watchable list operation.
- */
 type OperationHandler<K, V> = (key: K, value: V, prev?: V) => any;
-
-/**
- * Represents the handler function for a list update operation.
- */
 type OperationUpdateHandler<K, V> = (key: K, value: V, prev: V) => any;
-
-/**
- * Represents the map of operation listeners.
- */
 type OperationListeners<K, V> = {
     [op in Operation]: Set<OperationHandler<K, V>>;
 };
 
-/**
- * Represents the bulk watcher interface.
- */
 interface BulkWatcher<K, V> {
-    /**
-     * Registers a listener to be called whenever an update operation occurs.
-     */
     update?: OperationUpdateHandler<K, V>;
-
-    /**
-     * Registers a listener to be called whenever an insert operation occurs.
-     */
     insert?: OperationHandler<K, V>;
-
-    /**
-     * Registers a listener to be called whenever a delete operation occurs.
-     */
     delete?: OperationHandler<K, V>;
 }
 
@@ -100,31 +71,29 @@ export interface WatchableList<T> extends Watchable<number, T> {
     sort(fn?: (item: T, elem: T) => boolean): WatchableList<T>;
 }
 
-function subscribe<L>(set: Set<L>, listener: L): () => void {
-    set.add(listener);
-    return () => set.delete(listener);
+function subscribe<L>(set: Set<L>) {
+    return (listener: L) => {
+        set.add(listener);
+        return () => set.delete(listener);
+    };
 }
-
 
 /**
  * Converts an object into Watchable and return a trigger function to trigger the updates.
  */
 export function watchify<K, V>(list: any, unique: boolean = true): (op: Operation, key: K, value: V, prev?: V) => void {
-    const upd: OperationListeners<K, V> = { '+': new Set(), '-': new Set(), '=': new Set() };
+    const listeners: OperationListeners<K, V> = { '+': new Set(), '-': new Set(), '=': new Set() };
     const trigger = (op: Operation, key: K, value: V, updated?: V) => {
-        const handlers = upd[op];
+        const handlers = listeners[op];
         for (const handle of handlers || []) {
-            handle(key, value, updated);
+            if (op !== '=' || !unique || value !== updated) {
+                handle(key, value, updated);
+            }
         }
     };
-    list.onUpdate = (listener: OperationHandler<K, V>) =>
-        subscribe(upd['='], (key, value, prev) => {
-            if (!unique || prev !== value) {
-                listener(key, value, prev);
-            }
-        });
-    list.onInsert = (listener: OperationHandler<K, V>) => subscribe(upd['+'], listener);
-    list.onDelete = (listener: OperationHandler<K, V>) => subscribe(upd['-'], listener);
+    list.onUpdate = subscribe(listeners['=']);
+    list.onInsert = subscribe(listeners['+']);
+    list.onDelete = subscribe(listeners['-']);
     list.watch = (operations: BulkWatcher<K, V>) => {
         const unsubUpdate = operations.update && list.onUpdate(operations.update);
         const unsubInsert = operations.insert && list.onInsert(operations.insert);
