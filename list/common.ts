b5b2c7cc90d31cd0c8fd79c0469be3d7e8e25194
aldinh777 <aldi.nh789@gmail.com>
2025-01-10 02:09:29 +0800

    expose watchable in ./list/utils.ts and remove redundant comments

--- list/common.ts
+++ list/common.ts
@@ -3,73 +3,18 @@
  * Shared module for types and function related to watchability
  */
 
+export type OperationHandler<T> = (index: number, value: T, prev?: T) => any;
+export type OperationUpdateHandler<T> = (index: number, value: T, prev: T) => any;
+export interface BulkWatcher<T> {
+    update?: OperationUpdateHandler<T>;
+    insert?: OperationHandler<T>;
+    delete?: OperationHandler<T>;
+}
+
 type Operation = '+' | '-' | '=';
-type OperationHandler<K, V> = (key: K, value: V, prev?: V) => any;
-type OperationUpdateHandler<K, V> = (key: K, value: V, prev: V) => any;
-type OperationListeners<K, V> = {
-    [op in Operation]: Set<OperationHandler<K, V>>;
+type OperationListeners<T> = {
+    [op in Operation]: Set<OperationHandler<T>>;
 };
-
-interface BulkWatcher<K, V> {
-    update?: OperationUpdateHandler<K, V>;
-    insert?: OperationHandler<K, V>;
-    delete?: OperationHandler<K, V>;
-}
-
-/**
- * Represents a watchable object with operations to observe changes.
- */
-export interface Watchable<K, V> {
-    /**
-     * Registers a listener to be called whenever an update operation occurs.
-     */
-    onUpdate(listener: OperationUpdateHandler<K, V>): () => void;
-
-    /**
-     * Registers a listener to be called whenever an insert operation occurs.
-     */
-    onInsert(listener: OperationHandler<K, V>): () => void;
-
-    /**
-     * Registers a listener to be called whenever a delete operation occurs.
-     */
-    onDelete(listener: OperationHandler<K, V>): () => void;
-
-    /**
-     * Registers necessary listeners to observe multiple types of operations.
-     */
-    watch(operations: BulkWatcher<K, V>): () => void;
-}
-
-/**
- * Represents a watchable list with operations to observe changes.
- */
-export interface WatchableList<T> extends Watchable<number, T> {
-    /**
-     * Retrieves the current elements as array.
-     */
-    (): T[];
-
-    /**
-     * Retrieves the element at the specified index of the list.
-     */
-    (key: number): T;
-
-    /**
-     * Creates a new observed list that filters out the elements of the list based on the given function.
-     */
-    filter(fn: (item: T) => boolean): WatchableList<T>;
-
-    /**
-     * Creates a new observed list that maps the elements of the list based on the given function.
-     */
-    map<U>(fn: (item: T) => U): WatchableList<U>;
-
-    /**
-     * Creates a new observed list that sorts the elements of the list based on the given function.
-     */
-    sort(fn?: (item: T, elem: T) => boolean): WatchableList<T>;
-}
 
 function subscribe<L>(set: Set<L>) {
     return (listener: L) => {
@@ -81,9 +26,12 @@ function subscribe<L>(set: Set<L>) {
 /**
  * Converts an object into Watchable and return a trigger function to trigger the updates.
  */
-export function watchify<K, V>(list: any, unique: boolean = true): (op: Operation, key: K, value: V, prev?: V) => void {
-    const listeners: OperationListeners<K, V> = { '+': new Set(), '-': new Set(), '=': new Set() };
-    const trigger = (op: Operation, key: K, value: V, updated?: V) => {
+export function watchify<T>(
+    list: any,
+    unique: boolean = true
+): (op: Operation, index: number, value: T, prev?: T) => void {
+    const listeners: OperationListeners<T> = { '+': new Set(), '-': new Set(), '=': new Set() };
+    const trigger = (op: Operation, key: number, value: T, updated?: T) => {
         const handlers = listeners[op];
         for (const handle of handlers || []) {
             if (op !== '=' || !unique || value !== updated) {
@@ -94,7 +42,7 @@ export function watchify<K, V>(list: any, unique: boolean = true): (op: Operatio
     list.onUpdate = subscribe(listeners['=']);
     list.onInsert = subscribe(listeners['+']);
     list.onDelete = subscribe(listeners['-']);
-    list.watch = (operations: BulkWatcher<K, V>) => {
+    list.watch = (operations: BulkWatcher<T>) => {
         const unsubUpdate = operations.update && list.onUpdate(operations.update);
         const unsubInsert = operations.insert && list.onInsert(operations.insert);
         const unsubDelete = operations.delete && list.onDelete(operations.delete);
